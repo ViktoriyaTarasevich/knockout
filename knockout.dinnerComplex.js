7f760d3bb00f83acedb5d630407b24c0ce76d98e
ViktoriyaTarasevich <viktoriya.v.tarasevich@gmail.com>
2014-08-06 19:51:47 +0300

    [BugFix]: fixed total price computing.

--- knockout.dinnerComplex.js
+++ knockout.dinnerComplex.js
@@ -1,40 +1,33 @@
 'use strict';
 var LunchComplex = function(){
     var self = this;
-    self.foods = ko.computed(function() {
-            var items = [new Food()];
-            items = jsonData;
-            for(var i = 0; i < jsonData.length; i++){
-                items[i].title = jsonData[i].title;
-                items[i].image = jsonData[i].image;
-                items[i].price = jsonData[i].price;
-                items[i].selectedCount = 0;
-            }
-            return items;
-        });
+    self.foods = (function(){
+        var result = [];
+        for (var i = 0; i < jsonData.length; i++)
+        {
+            result.push({
+                title: jsonData[i].title,
+                price: ko.observable(jsonData[i].price),
+                image: jsonData[i].image,
+                isSelected: ko.observable(false),
+                selectedCount: ko.observable(0)
+            });
+        }
+        return result;
+    })()
 
     self.totalPrice = ko.computed(function() {
         var resultPrice = 0;
-        for (var i = 0; i < self.foods().length; i++){
-            resultPrice = resultPrice + (self.foods()[i].price *self.foods()[i].selectedCount) ;
+        for (var i = 0; i < self.foods.length; i++){
+            if (self.foods[i].selectedCount() > 0) {
+                resultPrice += (self.foods[i].price() * self.foods[i].selectedCount());
+            }
         }
         return resultPrice;
     });
-
-};
-
-var Food = function(){
-    var self = this;
-    self.title = ko.observable();
-    self.image = ko.observable();
-    self.price = ko.observable();
-    self.selectedCount = ko.observable();
-    self.isSelected = ko.computed(function(){
-        if(self.selectedCount > 0){
-            return true;
-        }
-        return false;
-    });
+    self.fillSelectElement = function(){
+        return 0;
+    };
 };
 
 $(document).ready(function() {
